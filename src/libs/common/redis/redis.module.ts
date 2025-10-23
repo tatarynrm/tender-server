d2517db70720fd8c3a72619fc039e3e640482144
tatarynrm <tatarynrm@gmail.com>
2025-10-23 14:48:51 +0300

    wwww

--- src/libs/common/redis/redis.module.ts
+++ src/libs/common/redis/redis.module.ts
@@ -8,9 +8,24 @@ import { createClient } from 'redis';
     {
       provide: 'REDIS_CLIENT',
       useFactory: async (configService: ConfigService) => {
-        const client = createClient({
-          url: configService.getOrThrow<string>('REDIS_URI'),
-        });
+        // const client = createClient({
+        //   url: configService.getOrThrow<string>('REDIS_URI'),
+        // });
+        // await client.connect();
+        // return client;
+
+                const user = configService.get<string>('REDIS_USER');
+        const password = configService.get<string>('REDIS_PASSWORD');
+        const host = configService.get<string>('REDIS_HOST');
+        const port = configService.get<string>('REDIS_PORT');
+
+        const url = password
+          ? `redis://${user}:${password}@${host}:${port}`
+          : `redis://${host}:${port}`;
+
+        console.log('✅ Redis URL:', url); // тимчасово для перевірки
+
+        const client = createClient({ url });
         await client.connect();
         return client;
       },
