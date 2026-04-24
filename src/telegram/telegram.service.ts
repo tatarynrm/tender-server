e604714d234dd6bbbf6ba4d5028c46fa2c50bf72
tatarynrm <tatarynrm@gmail.com>
2026-04-24 13:52:26 +0300

    eweqw

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -222,7 +222,7 @@ export class TelegramService implements OnModuleInit {
   }
 
   public async runDeploy(): Promise<{ success: boolean; output: string }> {
-    // вівdsadad
+    // вівdsadadewqe
     return new Promise((resolve) => {
       exec('deploy', (error, stdout, stderr) => {
         if (error) {
